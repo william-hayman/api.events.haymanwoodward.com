f3a7608c4e01d875d0ed805bb79922c56a9e4646
William Carriello <william.carriello@haymanwoodward.com>
2023-03-21 14:09:37 -0300

    Inicial Procfile

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+// use Laravel\Telescope\TelescopeApplicationServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
